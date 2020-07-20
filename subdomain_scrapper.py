13d2303d3e239e840b3956fe8c6452a46a56257a
altaf99 <iamaltafshaikh07@gmail.com>
2020-07-20 08:24:11 +0530

    added hacker target

--- subdomain_scrapper.py
+++ subdomain_scrapper.py
@@ -46,6 +46,7 @@ def find_subdomains_cert(domain):
 
     list_of_subdomains = string_data.split(',')
     print('[*] DONE Searching Cert for subdomains of %s' % domain)
+    print("[*] {0}: {1}".format(colored("Unique subdomains found", "cyan"), colored(len(set(list_of_subdomains)), "yellow")))
     return(set(list_of_subdomains))
 
 
@@ -62,6 +63,7 @@ def find_subdomains_censys(domain, api_id, api_secret):
             subdomains.extend(search_result['parsed.names'])
         
         print('[*] DONE Searching Censys for subdomains of %s' % domain)
+        print("[*] {0}: {1}".format(colored("Unique subdomains found", "cyan"), colored(len(set(subdomains)), "yellow")))
         return set(subdomains)
 
     except censys.base.CensysUnauthorizedException:
@@ -74,40 +76,6 @@ def find_subdomains_censys(domain, api_id, api_secret):
         # catch the Censys Base exception, example "only 1000 first results are available"
         sys.stderr.write('[-] Something bad happened, ' + repr(e))
         return set(subdomains)
-
-
-def find_subdomains_shodan(domain, shodan_api):
-    SD = [] 
-    api = shodan.Shodan(shodan_api)
-
-    if shodan_api == "":
-        print("  \__", colored("No Shodan API key configured", "red"))
-        return []
-
-    else:
-        try:
-            results = api.search("hostname:.{0}".format(domain))
-            print(results)
-            try:
-                for res in results["matches"]:
-                    SD.append("".join(res["hostnames"]))
-
-            except KeyError as errk:
-                print("  \__", colored(errk, "red"))
-                return []
-
-            SD = set(SD)
-
-            print("  \__ {0}: {1}".format(colored("Unique subdomains found", "cyan"), colored(len(SD), "yellow")))
-            return SD
-
-        except shodan.exception.APIError as err:
-            print("  \__", colored(err, "red"))
-            return []
-
-        except Exception:
-            print("  \__", colored("Something went wrong!", "red"))
-            return []
 
 def hacker_target(doamin):
     HT = []
@@ -126,7 +94,6 @@ def hacker_target(doamin):
                 HT.append(hostname)
 
         HT = set(HT)
-        print(HT)
         print("  \__ {0}: {1}".format(colored("Unique subdomains found", "cyan"), colored(len(HT), "yellow")))
         return HT
 
@@ -149,68 +116,6 @@ def hacker_target(doamin):
     except Exception:
         print("  \__", colored("Something went wrong!", "red"))
         return []
-
-        
-def cert_spotter_parseResponse(response, domain):
-    hostnameRegex = "([\w\.\-]+\.%s)" % (domain.replace(".", "\."))
-    hosts = findall(hostnameRegex, str(response))
-
-    return [host.lstrip(".") for host in hosts]
-
-def find_subdomains_cert_spotter(domain):
-    CS = []
-
-    print(colored("[*]-Searching CertSpotter...", "yellow"))
-
-    base_url = "https://api.certspotter.com"
-    next_link = "/v1/issuances?domain={0}&include_subdomains=true&expand=dns_names".format(domain)
-    # headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:52.0) Gecko/20100101 Firefox/52.0"}
-
-    while next_link:
-        try:
-            response = requests.get(base_url + next_link)
-
-            if response.status_code == 429:
-                print("  \__", colored("Search rate limit exceeded.", "red"))
-                return []
-
-            CS += cert_spotter_parseResponse(response.content, domain)
-
-            try:
-                next_link = response.headers["Link"].split(";")[0][1:-1]
-
-            except KeyError:
-                break
-
-        except requests.exceptions.RequestException as err:
-            print("  \__", colored(err, "red"))
-            return []
-
-        except requests.exceptions.HTTPError as errh:
-            print("  \__", colored(errh, "red"))
-            return []
-
-        except requests.exceptions.ConnectionError as errc:
-            print("  \__", colored(errc, "red"))
-            return []
-
-        except requests.exceptions.Timeout as errt:
-            print("  \__", colored(errt, "red"))
-            return []
-
-        except Exception:
-            print("  \__", colored("Something went wrong!", "red"))
-            return []
-
-    CS = set(CS)
-
-    print("  \__ {0}: {1}".format(colored("Unique subdomains found", "cyan"), colored(len(CS), "yellow")))
-    print(CS)
-    return CS
-
-## get unique subdomains
-def get_unique_subdomains():
-    pass
 
 
 # # Writing to file 
@@ -244,10 +149,8 @@ def main(domain, censys_api_id, censys_api_secret):
    
     # cert_subdomains = find_subdomains_cert(domain)
     # censys_subdomains = find_subdomains_censys(domain, censys_api_id, censys_api_secret)
-    # shodan_subdomains = find_subdomains_shodan(domain, shodan_api)
-    # cert_spotter_subdomains = find_subdomains_cert_spotter(domain)
-    hacker_target(domain)
-    # uniq_subdomains = cert_subdomains | censys_subdomains
+    hacker_target_subdomains = hacker_target(domain)
+    # uniq_subdomains = cert_subdomains | censys_subdomains | hacker_target_subdomains
    
     # save_subdomains_to_file("all_subdomains",domain,uniq_subdomains)
 
